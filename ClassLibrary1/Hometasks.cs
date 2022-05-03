9c97961a82d1dc8dca054ea5424e88de0f218f33
punderplus <ilya.obukhov.m@gmail.com>
2022-05-03 13:22:31 +0300

    3 tasks with tests

---
+++ ClassLibrary1/Hometasks.cs
@@ -0,0 +1,119 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+	[TestFixture]
+	public class Task_1
+	{
+		public List<object> GetIntegersFromList(List<object> mixedList)
+		{
+			List<object> numbersOnly = new List<object>();
+			for (int i = 0; i < mixedList.Count; i++)
+			{
+				if (mixedList[i].GetType() == typeof(int))
+				{
+					numbersOnly.Add(mixedList[i]);
+				}
+			}
+			return numbersOnly;
+		}
+		[Test]
+		public void GetIntegersFromListTest1()
+		{
+			List<object> expected = new List<object>() { 1, 2 };
+			List<object> actual = GetIntegersFromList(new List<object>() { 1, 2, 'a', 'b' });
+			Assert.AreEqual(expected, actual);
+		}
+		[Test]
+		public void GetIntegersFromListTest2()
+		{
+			List<object> expected = new List<object>() { 1, 2, 0, 15 };
+			List<object> actual = GetIntegersFromList(new List<object>() { 1, 2, 'a', 'b', 0, 15 });
+			Assert.AreEqual(expected, actual);
+		}
+		[Test]
+		public void GetIntegersFromListTest3()
+		{
+			List<object> expected = new List<object>() { 1, 2, 231 };
+			List<object> actual = GetIntegersFromList(new List<object>() { 1, 2, 'a', 'b', "aasf", '1', "123", 231 });
+			Assert.AreEqual(expected, actual);
+		}
+	}
+	[TestFixture]
+	public class Task_2
+	{
+		public char FirstNonRepeatingLetter(string str)
+		{
+			string stringHelper = str;
+			str = str.ToLower();
+			for (int i = 0; i < str.Length - 1; i++)
+			{
+				if (!((str.Substring(0, i) + str.Substring(i + 1)).Contains(str[i])))
+				{
+					return stringHelper[i];
+				}
+			}
+			return ' ';
+		}
+		[Test]
+		public void FirstNonRepeatingLetter1()
+		{
+			Assert.AreEqual('T', FirstNonRepeatingLetter("sTreSS"));
+		}
+		[Test]
+		public void FirstNonRepeatingLetter2()
+		{
+			Assert.AreEqual(' ', FirstNonRepeatingLetter("sTreSSret"));
+		}
+		[Test]
+		public void FirstNonRepeatingLetter3()
+		{
+			Assert.AreEqual('Z', FirstNonRepeatingLetter("Zaalluuppaa"));
+		}
+	}
+	public class Task_3
+	{
+		public int digital_root(int input)
+		{
+			if(input < 10)
+            {
+				return input;
+			}
+			char[] strInput = input.ToString().ToCharArray();
+			int sum = 0;
+			for(int i = 0; i < strInput.Length; i++)
+            {
+				Console.WriteLine(strInput[i]);
+				sum += Int32.Parse((strInput[i].ToString()));
+			}
+			return digital_root(sum);
+		}
+		[Test]
+		public void digital_root1()
+		{
+			Assert.AreEqual(7, digital_root(16));
+		}
+		[Test]
+		public void digital_root2()
+		{
+			Assert.AreEqual(6, digital_root(942));
+		}
+		[Test]
+		public void digital_root3()
+		{
+			Assert.AreEqual(6, digital_root(132189));
+		}
+		[Test]
+		public void digital_root4()
+		{
+			Assert.AreEqual(2, digital_root(493193));
+		}
+		[Test]
+		public void digital_root5()
+		{
+			Assert.AreEqual(0, digital_root(0));
+		}
+	}
+}
---
+++ ClassLibrary1/Workshops.cs
@@ -0,0 +1,123 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Workshop1
+    {
+        [Test]
+        public void fizzBuzz()
+        {
+            String actual = checkDivision(15);
+            Assert.AreEqual("Fizz Buzz", actual);
+        }
+        [Test]
+        public void buzz()
+        {
+            String actual = checkDivision(6);
+            Assert.AreEqual("Buzz", actual);
+        }
+        [Test]
+        public void fizz()
+        {
+            String actual = checkDivision(10);
+            Assert.AreEqual("Fizz", actual);
+        }
+        [Test]
+        public void nonFizzBuzz()
+        {
+            String actual = checkDivision(7);
+            Assert.AreEqual(null, actual);
+        }
+        public String checkDivision(int a)
+        {
+            if (a % 3 == 0 && a % 5 == 0)
+            {
+                return "Fizz Buzz";
+            }
+            if(a % 3 == 0)
+            {
+                return "Buzz";
+            }
+            if(a % 5 == 0)
+            {
+                return "Fizz";
+            }
+            return null;
+        }
+
+        [Test]
+        
+        public void checkDefault()
+        {
+            String actual = parseStr("#a#bc#d");
+            Assert.AreEqual("bd", actual);
+        }
+        [Test]
+        public void checkDoubleResh()
+        {
+            String actual = parseStr("#####a#bc#d");
+            Assert.AreEqual("bd", actual);
+        }
+
+        [Test]
+        public void checkDouble()
+        {
+            String actual = parseStr("a#bc#dftasd#p#r#t");
+            Assert.AreEqual("bdftast", actual);
+        }
+        [Test]
+        public void checkEmpty()
+        {
+            String actual = parseStr("");
+            Assert.AreEqual("", actual);
+        }
+        public String parseStr(String input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[0] == '#')
+                {
+                    input = input.Remove(0, 1);
+                    i--;
+                    continue;
+                }
+                if (input[i] == '#')
+                {
+                    input = input.Remove(i - 1, 2);
+                    i--;
+                }
+            }
+            return input;
+        }
+    }
+    [TestFixture]
+    public class Workshop2
+    {
+
+        [Test]
+        public void thursday()
+        {
+            DateTime day = new DateTime(2022, 5, 2);
+            Assert.AreEqual("08.05.2022", sunday(day));
+        }
+
+        [Test]
+        public void todayIsSunday()
+        {
+            DateTime day = new DateTime(2022, 4, 10);
+            Assert.AreEqual("17.04.2022", sunday(day));
+        }
+
+        public string sunday(DateTime day)
+        {
+            day = day.AddDays(1);
+            while (day.DayOfWeek != DayOfWeek.Sunday)
+            {
+                day = day.AddDays(1);
+            }
+            return day.ToString("dd.MM.yyyy");
+        }
+    }
+}
