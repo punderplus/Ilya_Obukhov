0cb7d0d2f72d85736c83187a93a5bdf39924dc38
punderplus <ilya.obukhov.m@gmail.com>
2022-05-23 00:51:38 +0300

    Added Page Object Pattern for Login Form

--- Selenium_Ilya_Obukhov/Selenium/UnitTest1.cs
+++ Selenium_Ilya_Obukhov/Selenium/UnitTest1.cs
@@ -2,26 +2,58 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
-
+//page object only for logining in. LoginForm class
 namespace Selenium
 {
+    //class to work with log in form
+    public class LoginForm
+    {
+        private readonly By LoginMenu = By.LinkText("Log in");
+        private readonly By UsernameField = By.Id("loginusername");
+        private readonly By PasswordField = By.Id("loginpassword");
+        private readonly By LoginButton = By.CssSelector("#logInModal .btn-primary");
+        
+        private readonly IWebDriver WebDriver;
+
+        public LoginForm(IWebDriver driver)
+        {
+            WebDriver = driver;
+        }
+        public void GoToLogin()
+        {
+            WebDriver.FindElement(LoginMenu).Click();
+        }
+        public LoginForm EnterUsername(string username)
+        {
+            WebDriver.FindElement(UsernameField).SendKeys(username);
+            return this;
+        }
+        public LoginForm EnterPassword(string password)
+        {
+            WebDriver.FindElement(PasswordField).SendKeys(password);
+            return this;
+        }
+        public LoginForm SumbitLogin()
+        {
+            WebDriver.FindElement(LoginButton).Click();
+            return new LoginForm(WebDriver);
+        }
+        public LoginForm Login(string username, string password)
+        {
+            GoToLogin();
+            Thread.Sleep(200);
+            EnterUsername(username);
+            Thread.Sleep(200);
+            EnterPassword(password);
+            Thread.Sleep(200);
+            return SumbitLogin();
+        }
+    }
     public class Tests
     {
         IWebDriver webDriver;
         ChromeOptions options;
-
-        public interface objectBuilder
-        {
-            void BuildPartA();
-
-            void BuildPartB();
-
-            void BuildPartC();
-        }
-        public Tests()
-        {
-            
-        }        
+     
         [SetUp]
         public void Setup()
         {
@@ -31,21 +63,12 @@ namespace Selenium
             //user credentials
             const string username = "punderplus";
             const string password = "ilia200210";
-
             //go to site
             webDriver.Navigate().GoToUrl("https://www.demoblaze.com");
+            //do logining in
+            LoginForm lf = new LoginForm(webDriver);
+            lf.Login(username, password);
 
-            //log in
-            webDriver.FindElement(By.LinkText("Log in")).Click();
-            Thread.Sleep(200);
-
-            //send credentials
-            webDriver.FindElement(By.Id("loginusername")).SendKeys(username);
-            Thread.Sleep(200);
-            webDriver.FindElement(By.Id("loginpassword")).SendKeys(password);
-            Thread.Sleep(400);
-            webDriver.FindElement(By.CssSelector("#logInModal .btn-primary")).Click();
-            Thread.Sleep(500);
         }
 
         [TearDown]
@@ -56,6 +79,7 @@ namespace Selenium
         }
 
         [Test]
+        
         public void LaptopTest()
         {
             //click laptops
@@ -108,60 +132,6 @@ namespace Selenium
 
             //quiting browser and shutting down web driver
         
-        }
-
-        [Test]
-        public void Test2()
-        {
-            //click laptops
-            Thread.Sleep(1000);
-            webDriver.FindElement(By.LinkText("Monitors")).Click();
-            Thread.Sleep(1000);
-
-            //choosing laptop
-            webDriver.FindElement(By.CssSelector("a[href *= 'prod.html?idp_=14']")).Click();
-            Thread.Sleep(1000);
-
-            //clicking add to cart button
-            webDriver.FindElement(By.CssSelector(" .btn-success")).Click();
-            Thread.Sleep(3000);
-
-            //clicking ok on alert
-            webDriver.SwitchTo().Alert().Accept();
-
-            //getting to cart list
-            Thread.Sleep(1000);
-            webDriver.FindElement(By.Id("cartur")).Click();
-            Thread.Sleep(1000);
-
-            //clicking on place order button
-            webDriver.FindElement(By.CssSelector(" .btn-success")).Click();
-            Thread.Sleep(200);
-            
-
-            //fill in place order form
-            webDriver.FindElement(By.Id("name")).SendKeys("Anto Kolisnik");
-            Thread.Sleep(100);
-            webDriver.FindElement(By.Id("country")).SendKeys("Ukraine");
-            Thread.Sleep(100);
-            webDriver.FindElement(By.Id("city")).SendKeys("City");
-            Thread.Sleep(100);
-            webDriver.FindElement(By.Id("card")).SendKeys("1234541265789898");
-            Thread.Sleep(100);
-            webDriver.FindElement(By.Id("month")).SendKeys("April");
-            Thread.Sleep(100);
-            webDriver.FindElement(By.Id("year")).SendKeys("2022");
-            Thread.Sleep(100);
-
-            //accept button
-            webDriver.FindElement(By.XPath("//button[contains(@onclick,'purchaseOrder')]")).Click();
-            Thread.Sleep(1000);
-
-            //checking if we have orderModal king of page object on screen
-            Assert.IsTrue(webDriver.FindElements(By.Id("orderModal")).Count == 1);
-
-            
-
         }
     }
 }
